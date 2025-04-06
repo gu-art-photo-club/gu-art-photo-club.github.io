600005faff246a805ece547ef2e5fdf763d6d73c
fukuto morita <gigunnphotos@gmail.com>
2025-04-07 04:30:55 +0900

    add gallery

--- src/data/safeWorks.ts
+++ src/data/safeWorks.ts
@@ -1,44 +1,856 @@
 export const works = [
   {
-    "image": "../assets/gallery/okmt_000.jpg",
-    "displayName": "anonymous",
+    "image": "../assets/gallery/akst_000.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_001.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_002.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_003.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_004.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_005.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_006.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_007.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_008.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_009.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_010.jpeg",
+    "displayName": "anonymous2",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_000.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_001.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_002.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_003.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_004.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_005.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_006.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_007.jpeg",
+    "displayName": "anonymous5",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_000.jpeg",
+    "displayName": "anonymous4",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_001.jpeg",
+    "displayName": "anonymous4",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_002.jpeg",
+    "displayName": "anonymous4",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_003.jpeg",
+    "displayName": "anonymous4",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_004.jpeg",
+    "displayName": "anonymous4",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_000.jpeg",
+    "displayName": "anonymous1",
     "tags": [
       "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_001.jpeg",
+    "displayName": "anonymous1",
+    "tags": [
+      "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_002.jpeg",
+    "displayName": "anonymous1",
+    "tags": [
+      "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_003.jpeg",
+    "displayName": "anonymous1",
+    "tags": [
+      "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_004.jpeg",
+    "displayName": "anonymous1",
+    "tags": [
+      "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_005.jpeg",
+    "displayName": "anonymous1",
+    "tags": [
+      "art"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_000.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_001.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_002.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_003.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_004.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_005.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_006.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_007.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_008.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_009.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/akst_010.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_000.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_001.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_002.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_003.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_004.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_005.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_006.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/mikk_007.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/nmiy_000.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/nmiy_001.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/nmiy_002.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/nmiy_003.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/nmiy_004.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/okmt_000.jpg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
     ]
   },
   {
     "image": "../assets/gallery/okmt_001.jpg",
-    "displayName": "anonymous",
+    "displayName": "anonymous3",
     "tags": [
-      "art"
+      "photo"
     ]
   },
   {
     "image": "../assets/gallery/okmt_002.jpg",
-    "displayName": "anonymous",
+    "displayName": "anonymous3",
     "tags": [
-      "art"
+      "photo"
     ]
   },
   {
     "image": "../assets/gallery/okmt_003.jpg",
-    "displayName": "anonymous",
+    "displayName": "anonymous3",
     "tags": [
-      "art"
+      "photo"
     ]
   },
   {
     "image": "../assets/gallery/okmt_004.jpg",
-    "displayName": "anonymous",
+    "displayName": "anonymous3",
     "tags": [
-      "art"
+      "photo"
     ]
   },
   {
     "image": "../assets/gallery/okmt_005.jpg",
-    "displayName": "anonymous",
+    "displayName": "anonymous3",
     "tags": [
-      "art"
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_000.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_001.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_002.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_003.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_004.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_005.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_006.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_007.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_008.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_009.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_010.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_011.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_012.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_013.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_014.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_015.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_016.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_017.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_018.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_019.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_020.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_021.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_022.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_023.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_024.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_025.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_026.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_027.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_028.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_029.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_030.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_031.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_032.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_033.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_034.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_035.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_036.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_037.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_038.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_039.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_040.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_041.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_042.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_043.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_044.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_045.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_046.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_047.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_048.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_049.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_050.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_051.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_052.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_053.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_054.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_055.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_056.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_057.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_058.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_059.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_060.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
+    ]
+  },
+  {
+    "image": "../assets/gallery/sijw_061.jpeg",
+    "displayName": "anonymous3",
+    "tags": [
+      "photo"
     ]
   }
 ];
